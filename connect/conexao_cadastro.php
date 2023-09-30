d8fc36edb9330df4848c884454c33bf553c4326c
Bianca Honda <yuka_honda@hotmail.com>
2023-10-01 01:27:00 +0900

    conexao com banco de dados

---
+++ connect/conexao_cadastro.php
@@ -0,0 +1,25 @@
+<?php 
+
+	$conn = mysqli_connect("localhost", "root", "", "bixcoito");
+		if ($conn === false) {
+			echo("Erro: não conectou."
+				. mysqli_connect_error());
+		}
+
+		$contausuario = !empty($_POST['contausuario']) ? $_POST['contausuario'] : '';
+		$nomeusuario = !empty($_POST['nomeusuario']) ? $_POST['nomeusuario'] : '';
+		$sobrenomeusuario = !empty($_POST['sobrenomeusuario']) ? $_POST['sobrenomeusuario'] : '';
+		$emailusuario = !empty($_POST['emailusuario']) ? $_POST['emailusuario'] : '';
+		$generousuario = !empty($_POST['genero']) ? $_POST['genero'] : '';
+		$senhausuario = !empty($_POST['senhausuario']) ? $_POST['senhausuario'] : '';
+
+		$sql = "INSERT INTO usuario (usuario, nome, sobrenome, email, genero, senha) values ('$contausuario', '$nomeusuario', '$sobrenomeusuario', '$emailusuario', '$generousuario', '$senhausuario')";
+
+		mysqli_query($conn, $sql);
+
+		mysqli_close($conn);
+
+		echo "'$emailusuario', '$contausuario', '$nomeusuario', '$sobrenomeusuario', '$generousuario', '$senhausuario'";
+
+		
+?>
