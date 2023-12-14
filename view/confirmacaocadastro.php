746f777df2d280ba4e0d026441afd3af1e42a7c0
Bianca Honda <yuka_honda@hotmail.com>
2023-12-15 02:58:30 +0900

    add pagina confirmacao email

---
+++ view/confirmacaocadastro.php
@@ -0,0 +1,24 @@
+<link rel="stylesheet" href="../css/confirmacaocadastro.css">
+<?php 
+include_once("header.php");	
+
+?>
+
+<body>
+    <div class="titulo"> <h1> Cadastro realizado com sucesso!</h1> </div>
+
+
+    <div class="texto"><text id="a">Para finalizar o processo, por favor, verifique seu e-mail e clique no link de confirmação que enviamos para você. <br>
+
+    Se não receber o e-mail dentro de alguns minutos, verifique sua pasta de spam ou entre em contato conosco para assistência. <br>
+
+    Estamos ansiosos para tê-lo(a) como parte da nossa comunidade! <br> <br>
+
+    Atenciosamente, <br>
+    Biscoiteiras </text>
+    </div>
+</body>
+
+<?php
+include_once("footer.php");
+?>
