bb8ffbdcfec00bd7a5d0da238a5606bf6de653e1
Bianca Honda <yuka_honda@hotmail.com>
2024-01-03 08:37:28 +0900

    page registration url didnt work

---
+++ view/confirmacao_falhou.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="../css/confirmacao_falhou.css">
+<?php 
+include_once("../view/header.php");	 
+?>
+
+<body>
+    <div class="titulo"> <h1> Algo deu errado...</h1> </div>
+
+
+    <div class="texto"><text id="a"> <p>Parece que houve um problema ao tentar confirmar o cadastro no nosso serviço com o e-mail fornecido.</p>
+    <p>Por favor, verifique se o link que você acessou está correto ou se já expirou. Se precisar, você pode tentar novamente.</p>
+    <p>Se este problema persistir ou se você acredita que isso foi um erro, entre em contato conosco para que possamos ajudar a resolver essa questão.</p>
+    <p>Lamentamos o inconveniente e agradecemos pela sua compreensão.</p>
+    <p>Atenciosamente,</p>
+    Biscoiteiras </text>
+    </div>
+</body>
+
+
+<?php 
+include_once("../view/footer.php");
+
+?>
