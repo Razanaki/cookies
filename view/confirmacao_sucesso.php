066744ab570ad806fca920e76e01560f320103d0
Bianca Honda <yuka_honda@hotmail.com>
2024-01-03 08:38:05 +0900

    page that shows a success message

---
+++ view/confirmacao_sucesso.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="../css/confirmacao_sucesso.css">
+<?php 
+include_once("../view/header.php");	 
+?>
+
+<body>
+    <div class="titulo"> <h1> Email confirmado com sucesso!</h1> </div>
+
+
+    <div class="texto"><text id="a">É com grande prazer que recebemos a confirmação do seu cadastro em nosso serviço. Seu registro foi concluído com sucesso e agora você faz parte da nossa comunidade!</p>
+    <p>Estamos empolgados em tê-lo(a) conosco e queremos assegurar que faremos o máximo para proporcionar uma experiência incrível.</p>
+    <p>Caso tenha alguma dúvida, sugestão ou feedback, não hesite em entrar em contato conosco. Estamos aqui para ajudar.</p>
+    <p>Muito obrigado por confiar em nós!</p>
+    <p>Atenciosamente,</p>
+    Biscoiteiras </text>
+    </div>
+</body>
+
+
+<?php 
+include_once("../view/footer.php");
+
+?>
