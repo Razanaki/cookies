fe1ab42aea605008e70ef4007fdcc2e8b0e6747b
Bianca Honda <yuka_honda@hotmail.com>
2024-04-11 04:56:43 +0900

    try and catch removed; login is case insensitive now

--- connect/conexao_login.php
+++ connect/conexao_login.php
@@ -5,38 +5,40 @@ $dbname = 'bixcoito';
 $username = 'root'; 
 $password = ''; 
 $pdo = new PDO("mysql:host=$host; dbname=$dbname", $username, $password);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Enable error reporting
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Check if 'usuariologin' and 'senhalogin' keys exist in $_POST array
+
     if (isset($_POST['usuariologin'], $_POST['senhalogin'])) {
 
-        $usuariologin = $_POST['usuariologin'];
+        $usuariologin = mb_strtolower($_POST['usuariologin'], "UTF-8");
         $senhalogin = $_POST['senhalogin'];
 		$senhausuario_hash = password_hash($senhalogin, PASSWORD_DEFAULT);
 
-        $checausuario = "SELECT COUNT(*) AS usuarioexiste FROM USUARIO WHERE USUARIO = ?";
+    
+        $checausuario = "SELECT CASEINSENSITIVE FROM USUARIO WHERE CASEINSENSITIVE = ?";
         $usuarioExiste = $pdo->prepare($checausuario);
         $usuarioExiste->execute([$usuariologin]);
         $usuarioResult = $usuarioExiste->fetchColumn();
 
-        $senhacorreta = "SELECT senha FROM USUARIO WHERE usuario = ?";
+        $senhacorreta = "SELECT senha FROM USUARIO WHERE CASEINSENSITIVE = ?";
         $senhaExiste = $pdo->prepare($senhacorreta);
         $senhaExiste->execute([$usuariologin]);
         $senhaResult = $senhaExiste->fetchColumn();
 
- 
-        try {
-            if ($usuarioResult == 1) {
+   
+
+   
+            if ($usuariologin == $usuarioResult ) {
                 echo "Usuário existe<br>"; 
                 echo "O usuário é " . $usuarioResult;
             
-                        if ((password_verify($senhalogin, $senhaResult)) > 0){
+                        if ((password_verify($senhalogin, $senhaResult)) == 1){
                         echo "<br>Senha correta!";
                         echo "<br>A senha correta é " . $senhaResult ;
                         echo "<br>A senha fornecida no login é " . $senhausuario_hash ;
                         }
-                                if ((password_verify($senhalogin, $senhaResult)) < 1) {
+                                if ((password_verify($senhalogin, $senhaResult)) != 1) {
                                     echo "<br>Senha incorreta!";
                                     echo "<br>A senha correta é " . $senhaResult ;
                                     echo "<br>A senha fornecida no login é " . $senhausuario_hash ;
@@ -44,17 +46,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                 
                     
-            } else if($usuarioResult == 0) {
+            } else  {
                 echo 'Usuário não existe';
-   
+                echo "<br>Usuário inserido: " .  $usuariologin ;
             }
-        
-        } catch (PDOException $e) {
-            echo "Error: " . $e->getMessage();
         }
-    } else {
-        
-        echo "Insira um usuário e senha existente.";
     }
-}
-
