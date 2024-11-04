7b717ae04f9c0f7f9f046ccff7f18dd67202f68b
Bianca Honda <yuka_honda@hotmail.com>
2024-11-04 19:37:32 +0900

    still figuring out how to set diff between dates for password

--- view/redefinesenha.php
+++ view/redefinesenha.php
@@ -2,11 +2,57 @@
 <?php include_once("header.php");
 date_default_timezone_set("Asia/Tokyo");
 
+
+$host = 'localhost';
+$dbname = 'bixcoito'; 
+$username = 'root'; 
+$password = ''; 
+$token = $_GET['token'];
+
+$pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+$checaHora = "SELECT COUNT(*) AS resultadotoken FROM USUARIO WHERE USUARIO_HASH = ?";
+$horaExiste = $pdo->prepare($checaHora);
+$horaExiste->execute([$token]);
+$horaResult = $horaExiste->fetchColumn();
+
+$pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+$host = 'localhost';
+$dbname = 'bixcoito'; 
+$username = 'root'; 
+$password = ''; 
+
+
+$horarioAtual = date('d-m-y h:i:s');
+
+$Teste1 = ($horaResult);
+$Teste2 = ($horarioAtual);
+$Teste3 = $Teste1->diff($Teste2);
+
+
+
+echo $Teste3->format('%R%a');
+
+
+
 ?>
 
-<header> <h1>Redefinir senha</h1></header>
 
-<body> </body>
+
+
+
+
+
+
+<header> <h1>Redefinir senha</h1>
+
+
+</header>
+
+
+<body> 
+
+</body>
 
 
 
